e1573556a02f51f2ef535a6f6072c0382781e202
kacha <83482486+kachavideo@users.noreply.github.com>
2024-05-20 21:43:59 +0800

    Update CKingJson.h

--- CKingJson.h
+++ CKingJson.h
@@ -15,12 +15,13 @@
 #include <vector>
 
 typedef struct kingJson {
-    kingJson *              pParent;
     std::string             strName;
     std::string             strData;
-    std::vector<kingJson>   lstItem;
-    std::vector<kingJson>   lstNode;
-    std::vector<kingJson>   lstList;
+    std::vector<kingJson*>  lstItem;
+    std::vector<kingJson*>  lstNode;
+    std::vector<kingJson*>  lstList;
+    kingJson *              pParent;
+    bool                    bString;
 } *PKINGJSON;
 
 class CKingJson {
@@ -29,11 +30,12 @@ public:
     virtual ~CKingJson(void);
 
     virtual int         ParseFile(const char* pFile, int nFlag);
-    virtual int         ParseData(const char* pData, int nSize);
+    virtual int         ParseData(const char* pData, int nSize, int nFlag);
 
     virtual PKINGJSON   GetRoot(void) { return &m_kjsRoot; }
     virtual PKINGJSON   FindNode(PKINGJSON pNode, const char* pName, bool bChild);
     virtual PKINGJSON   FindItem(PKINGJSON pNode, const char* pName, bool bChild);
+    virtual PKINGJSON   GetItem(PKINGJSON pNode, int nIndex);
 
     virtual const char* GetValue(PKINGJSON pNode, const char* pName);
     virtual const char* GetValue(PKINGJSON pNode, const char* pName, const char * pDefault);
@@ -47,32 +49,48 @@ public:
     virtual double      GetItemDbl(PKINGJSON pNode, int nIndex, double dDefault);
     virtual long long   GetItemLng(PKINGJSON pNode, int nIndex, long long lDefault);
 
-
     virtual PKINGJSON   AddNode(PKINGJSON pNode, const char* pName, bool bList);
     virtual PKINGJSON   AddItem(PKINGJSON pNode, const char* pName, const char * pValue);
     virtual PKINGJSON   AddItem(PKINGJSON pNode, const char* pName, int nValue);
     virtual PKINGJSON   AddItem(PKINGJSON pNode, const char* pName, double dValue);
     virtual PKINGJSON   AddItem(PKINGJSON pNode, const char* pName, long long lValue);
+    virtual PKINGJSON   AddItem(PKINGJSON pNode, int nIndex, const char* pValue, bool bString = true);
+    virtual PKINGJSON   AddItem(PKINGJSON pNode, int nIndex, int nValue);
+    virtual PKINGJSON   AddItem(PKINGJSON pNode, int nIndex, double dValue);
+    virtual PKINGJSON   AddItem(PKINGJSON pNode, int nIndex, long long lValue);
 
     virtual int         DelNode(PKINGJSON pNode, const char* pName, bool bList);
     virtual int         DelItem(PKINGJSON pNode, const char* pName);
+    virtual int         DelItem(PKINGJSON pNode, int nIndex);
+    virtual int         DelNode(PKINGJSON pNode);
 
     virtual PKINGJSON   ModItem(PKINGJSON pNode, const char* pName, const char* pValue);
     virtual PKINGJSON   ModItem(PKINGJSON pNode, const char* pName, int nValue);
     virtual PKINGJSON   ModItem(PKINGJSON pNode, const char* pName, double dValue);
     virtual PKINGJSON   ModItem(PKINGJSON pNode, const char* pName, long long lValue);
+    virtual PKINGJSON   ModItem(PKINGJSON pNode, int nIndex, const char* pValue);
+    virtual PKINGJSON   ModItem(PKINGJSON pNode, int nIndex, int nValue);
+    virtual PKINGJSON   ModItem(PKINGJSON pNode, int nIndex, double dValue);
+    virtual PKINGJSON   ModItem(PKINGJSON pNode, int nIndex, long long lValue);
 
     virtual const char* FormatText(void);
     virtual int         SaveToFile(const char * pFile);
 
 protected:
-    PKINGJSON       findNode(PKINGJSON pNode, const char* pName, bool bChild);
-    PKINGJSON       findItem(PKINGJSON pNode, const char* pName, bool bChild);
+    virtual PKINGJSON   findNode(PKINGJSON pNode, const char* pName, bool bChild);
+    virtual PKINGJSON   findItem(PKINGJSON pNode, const char* pName, bool bChild);
+    virtual int         delNode(PKINGJSON pNode, std::vector<kingJson*> * pList);
 
-    virtual int     parseNode(PKINGJSON pNode, char* pData, int nSize, bool bList);
-    virtual int     fillValue(std::string& strText, const char* pText);
+    virtual int         parseJson(PKINGJSON pNode, char* pData);
+    virtual int         parseText(char * pText, char** ppNext);
+    virtual int         parseData(char* pText, char** ppNext);
+    virtual PKINGJSON   apendNode(PKINGJSON pNode, const char* pName, bool bList);
+    virtual PKINGJSON   apendItem(PKINGJSON pNode, const char* pName, const char* pData, bool bString);
 
-    virtual int     formatText(PKINGJSON pNode, bool bList, bool bLast);
+    virtual int         formatText(PKINGJSON pNode, bool bList, bool bLast);
+    virtual int         removeChar(const char * pData, char * pText, int nSize);
+    virtual bool        isNodeList(PKINGJSON pNode);
+    virtual int         relaseNode(PKINGJSON pNode);
 
 protected:
     kingJson        m_kjsRoot;
